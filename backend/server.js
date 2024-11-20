39ef15974a311c09a3fd3890b62380d12799b28c
DMould123 <95115610+DMould123@users.noreply.github.com>
2024-11-20 20:33:51 +0100

    Updated the start and dev scripts in package.json to use cross-env for setting NODE_ENV.- Built the distribution files for hosting the project to add to my portfolio.

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,29 @@ import express from 'express'
 import dotenv from 'dotenv'
 import { connectDB } from './config/db.js'
 import shirtRoutes from './routes/shirt.routes.js'
+import path from 'path'
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
+const __dirname = path.resolve()
+
 // Middleware to parse JSON requests
 app.use(express.json())
-
-// Connect to the database
-connectDB()
 
 // Use the shirt routes under the /api prefix
 app.use('/api/shirts', shirtRoutes)
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/dist')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  })
+}
+
 app.listen(PORT, () => {
+  connectDB()
   console.log(`Server is running on PORT: ${PORT} 🚀`)
 })
